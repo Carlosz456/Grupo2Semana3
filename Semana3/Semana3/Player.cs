50cef9241ad541502cdc1e92c533ce04d25bf8bf
MjokHein <dany.f.uribe1404@gmail.com>
2026-09-10 18:57:41 -0500

    Create Class Player
    Daniel Uribe

---
+++ Semana3/Semana3/Player.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taller3Sem3
+{
+    internal class Player
+    {
+        protected float vida;
+        protected float dmg;
+
+        public virtual float RecibirDano(float dmgRecibido)
+        {
+            return vida - dmgRecibido;
+        }
+        public virtual float AtaqueDMG(float dmg)
+        {
+            return dmg;
+        }
+    }
+}
