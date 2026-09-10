50769bf6fe1823c4df2b5fea3735643c0f46e329
Carlosz456 <karloszp001@gmail.com>
2026-09-10 18:27:13 -0500

    Enemigo Rango

---
+++ Semana3/Semana3/EnemyRange.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grupo2Semana3
+{
+    internal class EnemyRange : Program
+    {
+        protected float vida;
+        protected float dano;
+        protected float balas;
+        public EnemyRange(float vida, float dano, float balas)
+        {
+            this.vida = vida;
+            this.dano = dano;
+            this.balas = balas;
+        }
+        public virtual float RecibirDanoEnemigoRango(float danoRecibido)
+        {
+            return vida - danoRecibido;
+        }
+
+        public virtual float AtaquedeEnemigoRango(float dano)
+        {
+            return dano;
+        }
+    }
+}
