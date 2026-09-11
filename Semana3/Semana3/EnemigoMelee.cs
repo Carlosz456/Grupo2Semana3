f6940cbb21233bec4594ef104791b74520103cb1
StephanieCamila <stephanie.camila00@gmail.com>
2026-09-10 19:24:38 -0500

    Arreglo override datos enemigo

--- Semana3/Semana3/EnemigoMelee.cs
+++ Semana3/Semana3/EnemigoMelee.cs
@@ -18,20 +18,20 @@ namespace Semana3
         }
         
         //Debe tener un método que le permita recibir daño
-        public void RecibirDañoEnemigo(float daño)
+        public override void RecibirDaño(float daño)
         {
             float enemigo_daño = vida - daño;
             vida = enemigo_daño;
         }
         
         //Debe tener un método que retorne el daño que puede causar 
-        public float CausarDañoEnemigo()
+        public override float CausarDaño()
         {
             return causarDaño;
         }
         
         //Debe tener un método que retorne si está vivo o muerto 
-        public bool EstadoEnemigoMelee()
+        public override bool EstadoEnemigo()
         {
             return vida > 0;
         }
